ccea94bac9d0ab33bea331c1669ed31beb8f6a2d
galabc <gabriel.laberge@polymtl.ca>
2024-11-22 10:53:21 -0500

    Include C Headers in the package.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, Extension
 
+TREEPATH = "pyfd/tree_shap"
+
 setup(
-    ext_modules=[Extension('pyfd.treeshap', sources=['pyfd/tree_shap/main.cpp'],),],
+    ext_modules=[Extension('pyfd.treeshap', sources=[f"{TREEPATH}/main.cpp"], 
+                           depends=[ f"{TREEPATH}/leaf_treeshap.hpp", f"{TREEPATH}/progressbar.hpp",
+                                     f"{TREEPATH}/recursive_treeshap.hpp", f"{TREEPATH}/utils.hpp",
+                                     f"{TREEPATH}/waterfall_treeshap.hpp"])]
+
 )
